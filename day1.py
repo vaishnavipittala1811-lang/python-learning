ad1ab5222942a633ca5d2a5b85e32511dba8e58c
vaishnavipittala1811-lang <vaishnavi.pittala1811@gmail.com>
2025-08-17 22:21:56 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,26 @@
+#Store your name & age in variables → print them.
+
+Take two numbers (a, b) → print sum, difference, product, division.
+
+name="vaishnavi"
+age=80
+a=13
+b=40
+print("additon",a+b)
+print("subration",a-b)
+print( "product",a*b)
+print("division",a/b)
+print(name)
+print(age)
+num=5
+print("cube",num**3)
+print("square",num**2)
+output:
+additon 53
+subration -27
+product 520
+division 0.325
+vaishnavi
+80
+cube 125
+square 25
