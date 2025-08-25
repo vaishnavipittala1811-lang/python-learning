abe07ce564558e66ecdadd2cb591605de1d34544
vaishnavipittala1811-lang <vaishnavi.pittala1811@gmail.com>
2025-08-25 23:15:18 +0530

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,25 @@
+#Sum, difference, product, and quotient of 15 and 4
+a = 15
+b = 4
+
+sum_result = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+
+print("Sum:", sum_result)
+print("Difference:", difference)
+print("Product:", product)
+print("Quotient:", quotient)
+#Find the remainder when 29 is divided by 5
+a = 29
+b = 5
+
+remainder = a % b
+print("Remainder:", remainder)
+#Find 3 to the power of 4
+base = 3
+exponent = 4
+
+result = base ** exponent
+print("3 to the power 4 is:", result)
